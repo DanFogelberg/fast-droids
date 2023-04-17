14b05f053430d595b3d6582fb5f23c6c5a20c6e0
DanFogelberg <dan.f.fogelberg@gmail.com>
2023-04-17 12:32:07 +0200

    Asteroid work

--- src/Asteroid/Asteroid.js
+++ src/Asteroid/Asteroid.js
@@ -1,12 +1,19 @@
 import '../css/Asteroid.css'; //Placeholder
 import { useState } from 'react';
+
+
+
 export default class Asteroid {
+    
 
-    position = {x, y}
-
-    // const [this.position, setPosition] = useState();
+    
 
     constructor() {
+        
+        const [position, setPosition] = useState({x: 0, y: 0});
+
+
+        setPosition({x: 5, y: 5});
         return (
           <div className="asteroid">
               *
