3f2d977784951684973c9d7f67a93b4e2e8c828b
DanFogelberg <dan.f.fogelberg@gmail.com>
2023-04-29 12:30:29 +0200

    Ship controls OK

--- src/Ship/Ship.js
+++ src/Ship/Ship.js
@@ -7,17 +7,19 @@ import styled from 'styled-components';
 const keys = {
     'w':false, 'a':false, 's':false, 'd':false,
 };
+const speedDecay = 0.95;
+
 
 class Ship extends React.Component{
 
     constructor(props){
         super(props);
-        this.state = {x: window.innerWidth/2, y: window.innerHeight/2, rotation: 0};
+        this.state = {x: window.innerWidth/2, y: window.innerHeight/2, rotation: 0}; //Rotation in rad
 
         this.xSpeed = 0;
         this.ySpeed = 0;
-        this.acceleration = 1;
-        this.rotationSpeed = 1;
+        this.acceleration = 0.5;
+        this.rotationSpeed = 0.1;
 
         window.addEventListener('keydown', (e) => {
             Object.keys(keys).forEach(key => {
@@ -40,7 +42,7 @@ class Ship extends React.Component{
 
         this.shipDiv = styled.div`
             color: red;
-            transform: translate(${this.state.x}px, ${this.state.y}px) rotate(${5}deg);
+            transform: translate(${this.state.x}px, ${this.state.y}px) rotate${this.state.rotation}rad);
             width: 100px;
             height: 100px;
             text-align: center;
@@ -54,7 +56,8 @@ class Ship extends React.Component{
         
         if(keys.w)
         {
-            
+            this.xSpeed =  this.xSpeed + this.acceleration * Math.sin(this.state.rotation);
+            this.ySpeed =  this.ySpeed - this.acceleration * Math.cos(this.state.rotation);
         }
         if(keys.a)
         {
@@ -62,19 +65,23 @@ class Ship extends React.Component{
         }
         if(keys.s)
         {
-            
+            this.xSpeed =  this.xSpeed - this.acceleration * Math.sin(this.state.rotation);
+            this.ySpeed =  this.ySpeed + this.acceleration * Math.cos(this.state.rotation);
         }
         if(keys.d)
         {
             this.setState({rotation: this.state.rotation + this.rotationSpeed});
         }
-
+        console.log(this.state.rotation);
+        
         this.setState({x: this.state.x + this.xSpeed});
-        this.setState({x: this.state.y + this.ySpeed});
+        this.setState({y: this.state.y + this.ySpeed});
+        this.xSpeed = this.xSpeed * speedDecay;
+        this.ySpeed = this.ySpeed * speedDecay;
 
         this.shipDiv = styled.div`
             color: red;
-            transform: translate(${this.state.x}px, ${this.state.y}px) rotate(${this.state.rotation}deg);
+            transform: translate(${this.state.x}px, ${this.state.y}px) rotate(${this.state.rotation}rad);
             width: 100px;
             height: 100px;
             text-align: center;
